f97476d0e4ab59ce96f3eaa9446060f3f04329a0
mahavir <mahavir37chandaliya@gmail.com>
2024-10-11 13:16:48 -0700

    vector_addition_cpuVSgpu

---
+++ Cuda-experiments/vector_addition/vadd.cu
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda_runtime.h>
+
+void vector_add_cpu() {
+    //pass
+}
+
+__global__ void vector_add_gpu() {
+    //pass
+}
+
+void init_vector() {
+    //pass
+}
+
+double get_time() {
+    //pass
+}
+
+int main() {
+    //pass
+
+    //Benchmark results
+}
